6ef4e37da328c9dd7977584b32e4ed41505bb870
Gourav Shah <gs@initcron.org>
2023-10-12 17:10:55 +0000

    move products to its own json file

--- catalogue/app.py
+++ catalogue/app.py
@@ -1,21 +1,15 @@
-from flask import Flask, jsonify
-from flask_cors import CORS, cross_origin
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 from datetime import datetime
 import socket
 import os
 import json
 
 app = Flask(__name__)
-CORS(app)
 
+# Load product data from JSON file
+with open('products.json', 'r') as f:
+    products = json.load(f)
 
-# Sample enriched products data
-products = [
-    {"id": 1, "name": "Craft Material A", "description": "High-quality material for crafting", "image_url": "/static/images/craft_material_a.jpg"},
-    {"id": 2, "name": "Craft Material B", "description": "Versatile material suitable for various crafts", "image_url": "/static/images/craft_material_a.jpg"},
-    {"id": 3, "name": "Craft Material C", "description": "Durable crafting material for sturdy creations", "image_url": "/static/images/craft_material_a.jpg"},
-]
 
 @app.route('/')
 def home():
