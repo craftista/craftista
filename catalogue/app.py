5808455e15c4e3cd690aa767c2d0efbb509eb511
Gourav Shah <gs@initcron.org>
2024-04-20 16:21:55 +0530

    Update app.py

--- catalogue/app.py
+++ catalogue/app.py
@@ -9,20 +9,35 @@ app = Flask(__name__)
 # Load product data from JSON file
 with open('products.json', 'r') as f:
     products = json.load(f)
+with open("config.json", "r") as config_file:
+    config_data = json.load(config_file) 
 
+def get_db_connection():
+    conn = psycopg2.connect(host=config_data.get("db_host"),
+                            database=config_data.get("db_name"),
+                            user=config_data.get("db_user"),
+                            password=config_data.get("db_password")
+    return conn
 
 @app.route('/')
 def home():
     system_info = get_system_info()
-    with open("config.json", "r") as config_file:
-      config_data = json.load(config_file) 
     app_version = config_data.get("app_version", "N/A")  # Default to "N/A" if no version is found
     return render_template('index.html', current_year=datetime.now().year, system_info=system_info, version=app_version)
 
 @app.route('/api/products', methods=['GET'])
 
 def get_products():
-    return jsonify(products), 200
+    if (config_data.get("data_source") == "db"): 
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM products;')
+        db_products = cur.fetchall()
+        cur.close()
+        conn.close()
+        return jsonify(db_products), 200
+    else
+        return jsonify(products), 200
 
 
 @app.route('/api/products/<int:product_id>', methods=['GET'])
